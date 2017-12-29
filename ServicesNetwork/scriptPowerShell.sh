f0919ec7f229340fa6d9902ce61336039a0135cc
sojnar <ranjos@globomail.com>
2017-12-29 12:18:04 +0000

    @8411df18

---
+++ ServicesNetwork/scriptPowerShell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Meu primeiro script!"
+echo -n "Digite seu nome: "
+read nome
+echo -n "Digite sua idade: "
+read idade
+echo "Seu nome é $nome e você tem $idade anos de idade!"
