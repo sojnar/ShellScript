812a15479e8a49d75e78b09b00f0eca7f6a62180
Rogério dos Anjos <ranjos@globomail.com>
2017-12-29 16:18:29 -0200

    Delete scriptPowerShell.sh

--- ServicesNetwork/scriptPowerShell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Meu primeiro script!"
-echo "Meu primeiro script!"
-echo -n "Digite seu nome: "
-read nome
-echo -n "Digite sua idade: "
-read idade
-echo "Seu nome é $nome e você tem $idade anos de idade!"
